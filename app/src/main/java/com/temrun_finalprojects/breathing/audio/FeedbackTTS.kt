a3a0917dea10b0e734f66d3ec55e4c06dafc7463
SAY-Hong <bobseahee@gmail.com>
2025-06-04 18:04:39 +0900

    FEAT: 호흡피드백 문구 -> TTS 반환

---
+++ app/src/main/java/com/temrun_finalprojects/breathing/audio/FeedbackTTS.kt
@@ -0,0 +1,24 @@
+package com.temrun_finalprojects.breathing.audio
+
+import android.content.Context
+import android.speech.tts.TextToSpeech
+import java.util.Locale
+
+
+class FeedbackTTS(context: Context?, listener: OnInitListener?) :
+    TextToSpeech(context, listener) {
+    init {
+        this.setPitch(1.0f) // 음성의 높낮이 설정
+        this.setSpeechRate(1.0f) // 음성의 속도 설정
+        this.setLanguage(Locale.KOREA)
+    }
+
+    fun speak(text: CharSequence?) {
+        this.speak(text, QUEUE_FLUSH, null, "id1")
+    }
+
+    fun destroy() {
+        this.stop()
+        this.shutdown()
+    }
+}
