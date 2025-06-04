7152e2d65254eedb7e7487dffc6883a7c9f157d3
SAY-Hong <bobseahee@gmail.com>
2025-06-05 00:38:28 +0900

    Comment: FeedbackTTS 주석추가

--- app/src/main/java/com/temrun_finalprojects/breathing/audio/FeedbackTTS.kt
+++ app/src/main/java/com/temrun_finalprojects/breathing/audio/FeedbackTTS.kt
@@ -1,10 +1,17 @@
 package com.temrun_finalprojects.breathing.audio
 
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.speech.tts.TextToSpeech
 import java.util.Locale
 import android.util.Log
+
+
+/**
+ * TTS(텍스트 음성 변환)를 통한 피드백 제공 클래스.
+ *
+ * @param context 애플리케이션 컨텍스트
+ * @param onReady TTS 초기화 완료 시 호출되는 콜백 함수
+ */
 
 class FeedbackTTS(context: Context, private val onReady: (() -> Unit)? = null) : TextToSpeech.OnInitListener {
     private var tts: TextToSpeech? = null
@@ -33,6 +40,15 @@ class FeedbackTTS(context: Context, private val onReady: (() -> Unit)? = null) :
         }
     }
 
+    /**
+     * 텍스트 -> 음성 출력
+     *
+     * - TTS 초기화 ❌,
+     * - 쿨타임 내에 호출,
+     * - 텍스트 비어 있는 경우 출력 ❌
+     *
+     * @param text 출력할 텍스트
+     */
     fun speak(text: String) {
         if (!isInitialized) {
             Log.w("FeedbackTTS", "TTS가 초기화되지 않았습니다.")
@@ -54,6 +70,10 @@ class FeedbackTTS(context: Context, private val onReady: (() -> Unit)? = null) :
         tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "utteranceId")
     }
 
+    /**
+     * TTS 리소스를 정리하고 종료.
+     * 액티비티 or 앱 종료 시 호출 필요.
+     */
     fun destroy() {
         tts?.stop()
         tts?.shutdown()
