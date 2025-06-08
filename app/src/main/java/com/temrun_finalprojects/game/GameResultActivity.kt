28ab8cbeb3230d59ff678c6d0df85d7f7769c066
Jihyeon1227 <jihyunkim12227@gmail.com>
2025-06-08 19:01:59 +0900

    FEAT: 게임 결과창 추가 & 점수 시스템 추가 & 판정 텍스트 추가

--- app/src/main/java/com/temrun_finalprojects/game/GameResultActivity.kt
+++ app/src/main/java/com/temrun_finalprojects/game/GameResultActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.temrun_finalprojects.MainActivity
 import com.temrun_finalprojects.R
 import com.temrun_finalprojects.RootActivity
 
